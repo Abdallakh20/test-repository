ec999eb512c508801c55f35dc885d672125010d8
Saracen <rebathash@gmail.com>
2023-02-24 10:47:41 +0300

    test-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const numbers = [- 3, 1, 17, 2, 3, -2, 4, -12, 5, 6, 22]
+let sum = 0
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 === 0) {
+//     sum = sum + numbers[i]
+//   }
+
+// }
+// console.log(sum)
+
+11
